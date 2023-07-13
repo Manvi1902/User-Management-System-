df21d76cb886a3ff6ced32ec7fe5123888774e0f
Manvi Dewangan <109518835+Manvi1902@users.noreply.github.com>
2023-07-13 13:50:41 +0530

    Add files via upload

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,1 +1,11 @@
+import React from "react";
 
+const Footer = () => {
+  return (
+    <div className="bg-dark text-white text-center p-4 mt-3">
+      <h2>USER MANAGEMENT</h2>
+    </div>
+  );
+};
+
+export default Footer;
